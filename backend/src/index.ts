fea14e94d3511c17c1c778f5e27e0bbdf737b92a
Ninjasri98 <palitsriparno@gmail.com>
2026-01-04 10:59:50 +0530

    set up mongo db

---
+++ backend/src/index.ts
@@ -0,0 +1,38 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import session from "cookie-session";
+import { config } from "./config/app.config";
+import connectDatabase from "./config/database.config";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    name: "session",
+    keys: [config.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000,
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite: "lax",
+  })
+);
+
+app.get(
+  `/`,
+  (async (req: Request, res: Response, next: NextFunction) => {
+    return res.status(200).json({
+      message: "Hello from Worknest Backend!",
+    });
+  })
+);
+
+app.listen(config.PORT, async () => {
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+  await connectDatabase();
+});

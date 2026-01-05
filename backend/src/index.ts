09c514136bec9a06cd16529af5064260387d0585
Ninjasri98 <palitsriparno@gmail.com>
2026-01-05 18:08:43 +0530

    added member routes and controllers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,8 @@ import { BadRequestException } from "./utils/appError";
 import authRoutes from "./routes/auth.route";
 import isAuthenticated from "./middleware/isAuthenticated.middleware";
 import userRoutes from "./routes/user.route";
+import workspaceRoutes from "./routes/workspace.route";
+import memberRoutes from "./routes/member.route";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -53,6 +55,8 @@ app.get(
 
 app.use(`${BASE_PATH}/auth`, authRoutes);
 app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
+app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
+app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
 
 app.use(errorHandler);
 
